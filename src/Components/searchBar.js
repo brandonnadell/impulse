0a7b2d6b9e58a5ae058c51e3e28eed8da808dd90
Akshay Heda <akshayheda@MacBook-Pro.local>
2020-07-21 20:39:16 -0700

    Added static background, updated search to handle urls

--- src/Components/searchBar.js
+++ src/Components/searchBar.js
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
-import styled from 'styled-components'
-import SearchBar from 'material-ui-search-bar'
+import SearchBar from 'material-ui-search-bar';
+import { isURL } from 'validator';
 
+
+const HTTP = "https://";
+const WWW = "www.";
+const SEARCH_URL = "http://www.google.com/search?q=";
 
 const searchHandler = (query) => {
-    window.location.href = "http://www.google.com/search?q=" + query;
+    let options = {"require_protocol": true};
+    if (isURL(query, options)) {
+        window.location.href = query;
+    }
+    else if (isURL(query)) {
+        console.log(query);
+        if (query.substring(0, 4) === WWW) {
+            window.location.href = HTTP + query;
+        }
+        else {
+            window.location.href = HTTP + WWW + query;
+        }
+    }
+    else {
+        window.location.href = SEARCH_URL + query;
+    }
 }
 const Search = (props) => {
     const [value, setValue] = useState("");
