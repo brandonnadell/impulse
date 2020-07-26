1f99a65c74d8207e5c74f99f57baae92bff477b2
Akshay Heda <akshayheda@MacBook-Pro.local>
2020-07-26 16:39:41 -0700

    changed styling, moved to digital clock

--- src/Components/Time.js
+++ src/Components/Time.js
@@ -1,7 +1,35 @@
-import React from 'react';
-import Clock from 'react-clock'
+import React, { useState, useEffect } from 'react';
+import Clock from 'react-digital-clock';
+import { Typography, Box } from '@material-ui/core';
+import styled from 'styled-components';
 
+const ClockWrapper = styled.div`
+    float: right;
+    padding: 8px 12px;
+`;
+const TIME_OPTIONS = {'hour12':true,'hour':'2-digit','minute':'2-digit'};
 const Time =()=>{
-    return <Clock />
+
+    const getCurrentTime = () => {
+        return  new Date().toLocaleTimeString([], TIME_OPTIONS);
+    }
+    const [time, setTime] = useState(getCurrentTime());
+
+    useEffect(() => {
+        setInterval(() => {
+            setTime(getCurrentTime());
+          }, 1000);
+    });
+    return (
+    <ClockWrapper>
+        <Typography variant='h5' display='inline'>
+            <Box fontWeight='fontWeightMedium' display='inline'>
+                {time}
+            </Box>
+        </Typography>
+    </ClockWrapper>
+    );
+    
+    
 }
 export default Time;
--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -2,9 +2,17 @@ import React, { useState, useEffect } from "react";
 import getWeather from '../util/getWeather';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import { Typography, Box } from "@material-ui/core";
+import styled from 'styled-components';
 
+const WeatherWrapper = styled.div`
+    float: right;
+    display: block;
+    padding: 0px 12px;
+    text-align: right;
+`;
 
-
+const DEGREE_SYMBOL = "&#176;"
 
 const Weather = (props) =>{
     const [position, setPosition] = useState(null)
@@ -19,21 +27,50 @@ const Weather = (props) =>{
         })
     }, [])
     console.log(weather)
+    if(weather) {
+        const city = weather.name;
+        const skyCover = weather.weather[0].main;
+        const temp = weather.temp;
+        return (  
+            <WeatherWrapper display='inline'>
+                <Typography variant='subtitle1'>
+                    <Box fontWeight='fontWeightMedium'>
+                        {city}
+                    </Box>
+                    <Box fontWeight='fontWeightLight'>
+                        {skyCover}
+                    </Box>
+                    <Box fontWeight='fontWeightLight'>
+                        {temp}{DEGREE_SYMBOL}
+                    </Box>
+                </Typography>
+            </WeatherWrapper>
+        );
+    }
     return (
-    <Card variant="elevated">
-        <CardContent>
-        {weather ?
-        <>
-        <div><p>Weather in {weather.name}:</p>
-        <p>{weather.weather[0].main}, temp: {weather.main.temp}</p></div>
-        </>
-        :
-        <div>Finding weather...</div>
-        }
-        </CardContent>
+        <WeatherWrapper>
+            <Typography variant='h6'>
+                <Box fontWeight='fontWeightMedium'>
+                    Loading Weather
+                </Box>
+            </Typography>
+        </WeatherWrapper>
+    );
+    // return (
+    // <Card>
+    //     <CardContent>
+    //     {weather ?
+    //     <>
+    //     <div><p>Weather in {weather.name}:</p>
+    //     <p>{weather.weather[0].main}, temp: {weather.main.temp}</p></div>
+    //     </>
+    //     :
+    //     <div>Finding weather...</div>
+    //     }
+    //     </CardContent>
 
-    </Card>
-    )
+    // </Card>
+    // )
 };
 
 export default Weather;
